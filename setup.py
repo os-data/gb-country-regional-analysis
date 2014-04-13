509816a046971be89a182a3ead5f1272d20d2a4e
Rufus Pollock <rufus.pollock@okfn.org>
2014-04-13 17:00:02 +0100

    [setup][xs]: make deps non-specific to versions.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version = '0.0.1',
     packages = find_packages(),
     install_requires = [
-        'datautil==0.4',
-        'xlrd==0.7.1'
+        'datautil',
+        'xlrd'
     ],
     entry_points = {
         'console_scripts': [
