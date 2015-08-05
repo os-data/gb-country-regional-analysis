3b4aaf270340576fce5491cba21473b52de68ae3
Rufus Pollock <rufus.pollock@okfn.org>
2015-08-05 08:42:41 +0100

    [agg][m]: aggregate by multiple columns working plus write of output to ./aggregates/ directory.
    * write 2 sample aggregates: by-department and by-cofog1-then-department

--- scripts/aggregate.py
+++ scripts/aggregate.py
@@ -39,30 +39,58 @@ def aggregate(reader, aggregate_by=None, sumon='amount', castrow=make_castrow())
     return output
 
 
+def doitall(aggregate_by, outname):
+    fo = open('data/cra.csv')
+    reader = csv.DictReader(fo)
+    out = aggregate(reader, aggregate_by)
+
+    # ok so where do headings come from - and do we need them?
+    outrows = [aggregate_by + ['value']]
+    outrows += [ [x for x in key] + [value] for key, value in out.items() ]
+
+    fo = open('aggregates/%s.csv' % outname, 'w')
+    writer = csv.writer(fo)
+    writer.writerows(outrows)
+
 def test_makecastrow():
     caster = make_castrow()
     out = caster({'amount': '10.0'})
     assert out == {'amount': 10.0}
 
-if __name__ == '__main__':
-    sumon = 'amount'
-
+def test_aggregate():
     fo = open('data/cra.csv')
     reader = csv.DictReader(fo)
     aggregate_by = [ 'cofog_level1_code' ]
     out = aggregate(reader, aggregate_by)
-    print out
 
+    fo = open('data/cra.csv')
+    reader = csv.DictReader(fo)
+    aggregate_by = [ 'cofog_level1_code', 'dept_code' ]
+    out = aggregate(reader, aggregate_by)
+    print sorted(out.keys())
+    print out[('02', 'Dept010')]
+    assert out[('02', 'Dept010')] == 10844376000.0
+
+
+if __name__ == '__main__':
+    doitall(['dept_code'], 'by-department')
+
+    doitall(['cofog_level1_code', 'dept_code'], 'by-cofog1-then-department')
+
+
+    # join example
     fo = open('data/cra.csv')
     reader = csv.DictReader(fo)
     aggregate_by = [ 'dept_code' ]
     out = aggregate(reader, aggregate_by)
-    print out
 
     # let's get fancy and join on departments
     departments = dict([x for x in csv.reader(open('data/departments.csv'))])
     dep_aggregates = ['dept_code', 'label', 'amount']
     dep_aggregates += [ [k[0], departments[k[0]], v] for k,v in out.items() ]
     import pprint
-    pprint.pprint(dep_aggregates)
+    # pprint.pprint(dep_aggregates)
 
+    # fo = open('aggregates/by-department.csv', 'w')
+    # writer = csv.writer(fo)
+    # writer.writerows(dep_aggregates)
